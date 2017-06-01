cccca95bf836ba617ff19b3918b5b6e6cb3a40de
phumai <kang081@hotmail.com>
2017-06-02 02:17:44 +0700

    update main.js

--- js/main.js
+++ js/main.js
@@ -22,5 +22,39 @@ console.log("Hello js");
 	});
 
 }());
+// ref to push 
+/*
+var massege={
+    text:"Robot club",
+    projectNumber:1
+};
+parload['massege/'+massegeKey]=massege;
+parload['long/'+massegeKey]=massege;
+
+//console.log(massege);
+//console.log(massegeKey);
+
+//update  insert
+//push  json 
+//set clrea on clea
+ref.update(parload);
+var logRef = ref.child('long');
+logRef.orderByKey().limitToLast(1).on('child_added',function(snap){
+  console.log('added  '+snap.val());
+});
+logRef.on('child_removed',function(snap){
+  console.log('removed  '+ snap.val());
+});
+ref.child('long').on('child_changed',function(snap){
+  console.log('changed  '+snap.val());
+});
+//query 
+
+ref.once('value')
+.then(function(snap){
+  console.log(snap.key ,"\n\n");
+  console.log(snap.ref.toString(), "\n\n");
+  console.log(snap.val() ,"\n\n");
+});*/
 
 
